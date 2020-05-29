2d11638b264dd07f5dc6ffb6657b5590fa77ada9
palaktank <101913785@student.swin.edu.au>
2020-05-30 00:26:10 +1000

    add header

--- src/App.js
+++ src/App.js
@@ -2,33 +2,17 @@ import React from "react";
 import "./App.css";
 import HomePage from "./component.pages/homepage/homepage.component";
 import ShopPage from "./component.pages/shop/shop.component";
-import { Route } from "react-router-dom";
-
-const HatsPage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>Hats Page</h1>
-    </div>
-  );
-};
-
-const Topic = () => (
-  <div>
-    <h1> Topic page</h1>
-  </div>
-);
-const TopicDetails = () => (
-  <div>
-    <h1> Topic Details page</h1>
-  </div>
-);
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage}></Route>
-      <Route path="/shop" component={ShopPage}></Route>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
